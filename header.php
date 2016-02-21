18cb076b13558ae1c5e0a18413aa7ad9e73ee65e
Tommy Chen <tommy351@gmail.com>
2016-02-22 01:09:41 +0800

    Implement responsive design

--- header.php
+++ header.php
@@ -2,7 +2,8 @@
 <html <?php language_attributes() ?>>
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
-  <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge, chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
   <?php wp_head() ?>
@@ -29,6 +30,10 @@
   <h1 class="header__logo-wrap">
     <a href="<?= esc_url(home_url('/')) ?>" class="header__logo"><?php bloginfo('name') ?></a>
   </h1>
+  <label class="header__menu-toggle" for="header__menu-checkbox">
+    <i class="fa fa-bars"></i>
+  </label>
+  <input id="header__menu-checkbox" type="checkbox">
   <nav class="header__nav">
     <?php wp_nav_menu(array('theme_location' => 'primary')) ?>
   </nav>
